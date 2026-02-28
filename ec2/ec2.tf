01f6e9d76dac0405cc6a5617189e788b5040c41b
suma <suma.vadla18@gmail.com>
2026-02-28 20:42:29 +0530

    actions

---
+++ ec2/ec2.tf
@@ -0,0 +1,33 @@
+resource "aws_instance" "exampe" {
+    ami = "ami-0220d79f3f480ecf5"
+    instance_type = "t3.micro"
+
+    tags = {
+        Name = "terraform"
+        Project = "roboshop"
+    }
+}
+
+resource "aws_security_group" "allow_tls" {
+
+    description = "Allow TLS inbound traffic and all outbound traffic "
+
+    egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  } 
+   
+    ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+    tags={ 
+        Name = "allow-all-terraform"
+    }
+}
